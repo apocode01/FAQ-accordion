95aff8b5e997b43db169660f4d8ed83d87fa3eb4
apocode01 <apopanou@gmail.com>
2025-09-02 14:34:09 +0300

    transitions

--- script.js
+++ script.js
@@ -1,13 +1,24 @@
-const answers = document.querySelectorAll(`.answer`);
 const buttons = document.querySelectorAll(`.button`);
-
-answers.forEach(answer => {
-    answer.classList.add('hidden');
-})
 
 buttons.forEach((button, index) => {
     button.addEventListener('click', () => {
         const answer = document.getElementById(`answer${index+1}`);
-        answer.classList.toggle('hidden');
+        const plus = document.getElementById(`plus${index+1}`);
+        const minus = document.getElementById(`minus${index+1}`);
+        
+        if (answer.style.maxHeight) {
+            // collapse
+            answer.style.maxHeight = null;
+            answer.classList.remove("open");
+            plus.style.display = "block";
+            minus.style.display = "none";
+            } 
+        else {
+            // expand
+            answer.style.maxHeight = answer.scrollHeight + "px";
+            answer.classList.add("open");
+            plus.style.display = "none";
+            minus.style.display = "block";
+        }
     })
 })
